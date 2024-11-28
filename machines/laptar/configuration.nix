3e679968fc25bd8d38a12bdb9a6677f363e35b04
j4ns8i <jsmalkowski+github@gmail.com>
2024-11-28 14:48:10 -0500

    nix: add j4ns8i to trusted users on nixos

--- machines/laptar/configuration.nix
+++ machines/laptar/configuration.nix
@@ -113,6 +113,8 @@
     };
   };
 
+  nix.settings.trusted-users = [ "root" "j4ns8i" ];
+
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
