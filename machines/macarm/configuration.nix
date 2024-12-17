9ce27a34c66bd6c27b1e71673973435e4d95eeef
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-17 09:40:37 -0500

    macarm: add user profile to nix.settings.trusted-users

--- machines/macarm/configuration.nix
+++ machines/macarm/configuration.nix
@@ -132,6 +132,8 @@
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
+  nix.settings.trusted-users = [ "root" "justin.smalkowski" ];
+
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
 
