12a0bd30cf606430ce773b0705c41c6b4a195a7b
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-28 17:42:10 -0500

    proton: add temporary dmraid overlay until next nixos-unstable release
    See https://github.com/NixOS/nixpkgs/pull/368470

--- machines/proton/configuration.nix
+++ machines/proton/configuration.nix
@@ -183,6 +183,20 @@
     enable = true;
   };
 
+  nixpkgs.overlays = [
+    # see https://github.com/NixOS/nixpkgs/pull/368470#pullrequestreview-2524454758
+    (final: prev: {
+      dmraid = prev.dmraid.overrideAttrs (oA: {
+        patches = oA.patches ++ [
+          (prev.fetchpatch2 {
+            url = "https://raw.githubusercontent.com/NixOS/nixpkgs/f298cd74e67a841289fd0f10ef4ee85cfbbc4133/pkgs/os-specific/linux/dmraid/fix-dmevent_tool.patch";
+            hash = "sha256-MmAzpdM3UNRdOk66CnBxVGgbJTzJK43E8EVBfuCFppc=";
+          })
+        ];
+      });
+    })
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
