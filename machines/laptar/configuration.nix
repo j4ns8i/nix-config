194648e5832e646608f60798e8f513575de7cd90
j4ns8i <jsmalkowski+github@gmail.com>
2026-06-03 22:13:04 -0400

    laptar: remove tlp thresholds since hardware doesn't seem to support them
    This is going off of the following observations:
    
    1. With this set, charging still went past 80%
    2. The charge control settings seem borked:
    
    ```
    $ cat /sys/class/power_supply/BAT0/charge_full
    3174000
    
    $ cat /sys/class/power_supply/BAT0/charge_control_start_threshold
    cat: /sys/class/power_supply/BAT0/charge_control_start_threshold: Input/output error
    
    $ cat /sys/class/power_supply/BAT0/charge_control_end_threshold
    cat: /sys/class/power_supply/BAT0/charge_control_end_threshold: Input/output error
    ```

--- machines/laptar/configuration.nix
+++ machines/laptar/configuration.nix
@@ -241,15 +241,6 @@
 
   networking.hostName = config.dotfiles.general.hostname;
 
-  services.power-profiles-daemon.enable = false;
-  services.tlp = {
-    enable = true;
-    settings = {
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
-      RESTORE_THRESHOLDS_ON_BAT = 1;
-    };
-  };
 }
 
 # vim: set sw=2 :
